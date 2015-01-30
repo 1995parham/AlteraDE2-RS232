4d3a60f55ed04424d6e817e262a7fd817edbebf3
Parham Alvani <parham.alvani@gmial.com>
2015-01-30 10:19:59 +0330

    state changing method improved.

--- src/main.vhd
+++ src/main.vhd
@@ -4,7 +4,7 @@ use ieee.std_logic_1164.all;
 entity serial is port (
 	rxd : in std_logic := '1';
 	txd : out std_logic := '1';
-	led : out std_logic;
+	led : out std_logic := '0';
 	clk : in std_logic
 );
 end entity;
@@ -37,63 +37,71 @@ begin
 	snd: async_transmitter port map (clk, snd_start, snd_data, txd, snd_busy);
 
 	process(clk) begin
-		if (clk'event and clk = '1') then
-			case present_state is
-				when s1 =>
-					if (rxd = '0') then
-						present_state <= recv;
-					end if;
-				when recv =>
-					-- nothing
-				when s2 =>
-					if (input(7 downto 0) = "00001010") then
-						present_state <= s3;
-					else
-						player_color(7 downto 0) <= input(7 downto 0);
-						player_color(15 downto 8) <= player_color(7 downto 0);
-						present_state <= s2;
-					end if;
-				when s3 =>
-					-- start send @
-					snd_data <= "01000000";
-					snd_start <= '1';
-					present_state <= send1;
-				when send1 =>
-					if (snd_busy = '0') then
-						present_state <= s4;
-						snd_start <= '0';
-					end if;
-				when s4 =>
-					-- start send 0
-					snd_data <= "00110000";
-					snd_start <= '1';
-					present_state <= send2;
-				when send2 =>
-					if (snd_busy = '0') then
-						present_state <= s5;
-						snd_start <= '0';
-					end if;
-				when s5 =>
-					-- start send /
-					snd_data <= "00101111";
-					snd_start <= '1';
-					present_state <= send3;
-				when send3 =>
-					if (snd_busy = '0') then
-						present_state <= s6;
-						snd_start <= '0';
-					end if;
-				when s6 =>
-					-- start send \n
-					snd_data <= "00001010";
-					snd_start <= '1';
-					present_state <= send4;
-				when send4 =>
-					if (snd_busy = '0') then
-						present_state <= s3;
-						snd_start <= '0';
-					end if;
-			end case;
+		if (rising_edge(clk)) then
+			present_state <= next_state;
 		end if;
+	end process;
+	process(present_state) begin
+		case present_state is
+			when s1 =>
+				if (rxd = '0') then
+					present_state <= recv;
+				end if;
+			when recv =>
+				-- nothing
+				present_state <= s3;
+			when s2 =>
+				if (input(7 downto 0) = "00001010") then
+					present_state <= s3;
+				else
+					player_color(7 downto 0) <= input(7 downto 0);
+					player_color(15 downto 8) <= player_color(7 downto 0);
+					present_state <= s2;
+				end if;
+			when s3 =>
+				-- start send @
+				snd_data <= "01000000";
+				snd_start <= '1';
+				next_state <= send1;
+			when send1 =>
+				if (snd_busy = '0') then
+					next_state <= s4;
+				else
+					snd_start <= '0';
+				end if;
+			when s4 =>
+				-- start send 0
+				snd_data <= "00110000";
+				snd_start <= '1';
+				next_state <= send2;
+			when send2 =>
+				if (snd_busy = '0') then
+					next_state <= s5;
+				else
+					snd_start <= '0';
+				end if;
+			when s5 =>
+				-- start send /
+				snd_data <= "00101111";
+				snd_start <= '1';
+				next_state <= send3;
+			when send3 =>
+				if (snd_busy = '0') then
+					next_state <= s6;
+				else
+					snd_start <= '0';
+				end if;
+			when s6 =>
+				-- start send \n
+				snd_data <= "00001010";
+				snd_start <= '1';
+				next_state <= send4;
+			when send4 =>
+				if (snd_busy = '0') then
+					next_state <= s3;
+				else
+					snd_start <= '0';
+				end if;
+		end case;
 	end process;
 end main_arch;
