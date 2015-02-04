bbe34b9cbe6b0147ab4272ccf8e17c22a6c35283
Parham Alvani <parham.alvani@gmial.com>
2015-02-05 02:05:45 +0330

    Some syntax error corrected.

--- src/main.v
+++ src/main.v
@@ -16,50 +16,58 @@ module serial (rxd, txd, clk);
 	reg snd_busy;
 
 	/* set senders port */
-	snd: async_transmitter port map (clk, snd_start, snd_data, txd, snd_busy);
+	async_transmitter snd(clk, snd_start, snd_data, txd, snd_busy);
 	
 	always @(posedge clk) begin
 		present_state = next_state;
 	end
 	
 	always @(present_state, snd_busy) begin
-		case present_state
-			s1:
+		case (present_state)
+			0: begin
 				/* start send @ */
 				snd_data = 8'b0100_0000;
 				snd_start = 1'b1;
-				next_start = send1
-			send1:
+				next_start = send1;
+			end
+			1: begin
 				snd_start <= 1'b0;
 				if (snd_busy == 1'b0)
 					next_state = s2;
-			s2:
+			end
+			2: begin
 				/* start send 0 */
 				snd_data = 8'b0011_0000;
 				snd_start = 1'b1;
 				next_state = send2;
-			send2:
+			end
+			3: begin
 				snd_start <= 1'b0;
 				if (snd_busy == 1'b0)
 					next_state = s3;
-			s3:
+			end
+			4: begin
 				/* start send / */
 				snd_data = 8'b0010_1111;
 				snd_start = 1'b1;
 				next_state = send3;
-			send3:
+			end
+			5: begin
 				snd_start <= 1'b0;
 				if (snd_busy == 1'b0)
 					next_state = s4;
-			s4:
+			end
+			6: begin
 				/* start send \n */
 				snd_data = 8'b0000_1010;
 				snd_start = 1'b1;
 				next_state = send4;
-			send4:
+			end
+			7: begin
 				snd_start <= 1'b0;
 				if (snd_busy == 1'b0)
 					next_state = s1;
+			end
 		endcase
 	end
 endmodule
